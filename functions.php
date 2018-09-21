de21b0eba0e61f3a9422be056b4ca3d07fc82444
renji333 <francischristanval@outlook.fr>
2018-09-21 17:44:57 +0200

    Bug Fixes Toc Nlp => NLP & Folder unmixed

--- functions.php
+++ functions.php
@@ -17,40 +17,45 @@ function getAllCategorieSlug($e){
 
 function writeNlpToc($e,$link){
 
-    $posts = get_posts( array(
-        'numberposts'    => 10,
-        'category'       => $e
-    ) );
+    $categories = get_categories(array( 'parent' => $e ));
 
-    if ( $posts ) {
+    if($categories){
 
-        foreach ($posts as $p) {
+        foreach ($categories as $c) {
 
-            if($p->guid == $link){
-                $class = " class=' Selected '";
-            }else{
-                $class = "";
-            }
+            echo "<li><span>$c->cat_name</span><ul>";
 
-            echo "<li $class>
+            writeNlpToc($c->term_id,$link);
+
+            echo "</ul></li>";
+
+        }
+
+    }else{
+
+        $posts = get_posts( array(
+            'numberposts'    => 10,
+            'category'       => $e
+        ) );
+
+        if ( $posts ) {
+
+            foreach ($posts as $p) {
+
+                if($p->guid == $link){
+                    $class = " class=' Selected '";
+                }else{
+                    $class = "";
+                }
+
+                echo "<li $class>
                     <a href='".$p->guid."'>".$p->post_title."</a>
                   </li>";
+
+            }
 
         }
 
     }
-
-    $categories = get_categories(array( 'parent' => $e ));
-
-    foreach ($categories as $c) {
-
-        echo "<li><span>$c->cat_name</span><ul>";
-
-        writeNlpToc($c->term_id,$link);
-
-        echo "</ul></li>";
-
-    }
-
 
 }
